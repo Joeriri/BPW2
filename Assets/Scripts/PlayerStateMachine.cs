4d31aed339f11eec7f336e867f61ea933d9dbdd7
Joeri Ridder <joeriridder@LIVE.NL>
2019-06-05 18:27:52 +0200

    Player State Machine + car usage reworked

---
+++ Assets/Scripts/PlayerStateMachine.cs
@@ -0,0 +1,176 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStateMachine : MonoBehaviour
+{
+    // Player
+    private UnityStandardAssets.Characters.FirstPerson.FirstPersonController fpc;
+    private CharacterController cc;
+    private Camera cam;
+    private Rigidbody rb;
+
+    [Header("Car")]
+    private UnityStandardAssets.Vehicles.Car.CarController car;
+    private UnityStandardAssets.Vehicles.Car.CarUserControl carUserControl;
+    [SerializeField] private Camera carCam;
+    private CarExitPoint carExitPoint;
+    [SerializeField] private float carAccesRange = 10f;
+
+    // Sky
+    [Header("Stargazing")]
+    [SerializeField] private Canvas sky;
+
+    public enum PlayerStates
+    {
+        Walking,
+        Driving,
+        Stargazing
+    }
+    public PlayerStates state;
+
+    private void Awake()
+    {
+        // Player
+        fpc = GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>();
+        cam = GetComponentInChildren<Camera>();
+        cc = GetComponent<CharacterController>();
+        rb = GetComponent<Rigidbody>();
+        // Car
+        car = FindObjectOfType<UnityStandardAssets.Vehicles.Car.CarController>();
+        carUserControl = car.GetComponent<UnityStandardAssets.Vehicles.Car.CarUserControl>();
+        carExitPoint = car.GetComponentInChildren<CarExitPoint>();
+
+    }
+
+    private void Start()
+    {
+        state = PlayerStates.Walking;
+    }
+
+    void Update()
+    {
+        SwitchState();
+    }
+
+    void SwitchState()
+    {
+        switch (state)
+        {
+            case PlayerStates.Walking:
+                WalkState();
+                break;
+            case PlayerStates.Driving:
+                CarState();
+                break;
+            case PlayerStates.Stargazing:
+                StargazeState();
+                break;
+        }
+    }
+
+    // De speler is niet aan de sterren aan het kijken
+    void WalkState()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            EnterTelescopeState();
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            RaycastHit hit;
+            Debug.DrawRay(cam.transform.position, cam.transform.forward * carAccesRange, Color.red);
+            if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, carAccesRange))
+            {
+                if (hit.collider.transform.root.gameObject == car.gameObject)
+                {
+                    EnterCarState();
+                }
+            }
+        }
+    }
+
+    // De speler zit in de auto
+    void CarState()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            ExitCarState();
+        }
+    }
+
+    void EnterCarState()
+    {
+        state = PlayerStates.Driving;
+        // Child player
+        transform.parent = car.transform;
+        transform.position = car.transform.position;
+        // Enable car
+        carUserControl.enabled = true;
+        carCam.gameObject.SetActive(true);
+        car.Move(0, 0, 0, 0); // Disable handbreak
+        // Disable player
+        fpc.enabled = false;
+        cc.enabled = false;
+        cam.gameObject.SetActive(false);
+    }
+
+    void ExitCarState()
+    {
+        state = PlayerStates.Walking;
+        // De-child player
+        transform.position = carExitPoint.transform.position;
+        transform.parent = null;
+        transform.LookAt(car.transform);
+        // Disable car
+        carUserControl.enabled = false;
+        carCam.gameObject.SetActive(false);
+        car.Move(0, 0, 0, 1); // Enable handbreak
+        // Enable player
+        fpc.enabled = true;
+        cc.enabled = true;
+        cam.gameObject.SetActive(true);
+    }
+
+    // De speler is naar de sterren aan het kijken
+    void StargazeState()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            ExitTelescopeState();
+        }
+    }
+
+    void EnterTelescopeState()
+    {
+        state = PlayerStates.Stargazing;
+        fpc.enabled = false;
+        car.enabled = false;
+        sky.gameObject.SetActive(true);
+
+        //StartCoroutine(LookUp());
+    }
+
+    void ExitTelescopeState()
+    {
+        state = PlayerStates.Walking;
+        fpc.enabled = true;
+        car.enabled = true;
+        sky.gameObject.SetActive(false);
+    }
+
+    IEnumerator LookUp(float duration)
+    {
+        float t = 0;
+        float step = 1f / duration;
+        while (t < 1)
+        {
+
+        }
+
+        //playerCam.gameObject.transform.rotation = Quaternion.EulerAngles
+
+        yield return null;
+    }
+}
--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -2,75 +2,154 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// SOURCE CODE: https://answers.unity.com/questions/734210/tiling-problem-with-perlin-generated-terrain-chunk.html
+
 public class TerrainGenerator : MonoBehaviour
 {
-    private Terrain terrain;
+    [SerializeField] private Vector2 mapSize = new Vector2(256, 256);
+    [SerializeField] private float maxHeight = 15f;
+    [SerializeField] private float scale = 5f;
+    [SerializeField] private Vector2 noiseOffset = new Vector2(100, 100);
 
-    [Header("Terrain")]
-    [SerializeField] private int width = 256;
-    [SerializeField] private int height = 256;
-    [SerializeField] private int depth = 20;
-    [SerializeField] private float scale = 20f;
-    [SerializeField] private float offsetX = 0f;
-    [SerializeField] private float offsetY = 0f;
-
-    [Header("Player")]
-    [SerializeField] private Player player;
+    [SerializeField] private UnityStandardAssets.Characters.FirstPerson.FirstPersonController player;
     private Vector2 playerTile = Vector2.zero;
 
-    private void Awake()
+    void Start()
     {
-        terrain = GetComponent<Terrain>();
-    }
-
-    private void Start()
-    {
-        terrain.terrainData = GenerateTerrain(terrain.terrainData);
-    }
-
-    private void Update()
-    {
-
-        if (Mathf.Floor(player.transform.position.x / width) != playerTile.x || Mathf.Floor(player.transform.position.z / height) != playerTile.y)
+        // Genereer de woestijn!
+        for (int x = -1; x <= 1; x++)
         {
-            playerTile = new Vector2(Mathf.Floor(player.transform.position.x / width), Mathf.Floor(player.transform.position.z / height));
-            transform.position = new Vector3(playerTile.x * width, 0, playerTile.y * height);
-            offsetX = playerTile.x * width;
-            offsetY = playerTile.x * height;
-            terrain.terrainData = GenerateTerrain(terrain.terrainData);
-
-            Debug.Log(playerTile);
+            for (int y = -1; y <= 1; y++)
+            {
+                GenerateMesh(new Vector2(playerTile.x + x, playerTile.y + y));
+            }
         }
     }
 
-    TerrainData GenerateTerrain(TerrainData terrainData)
+    void Update()
     {
-        terrainData.heightmapResolution = width + 1;
-        terrainData.size = new Vector3(width, depth, height);
-        terrainData.SetHeights(0, 0, GenerateHeights());
-        return terrainData;
+        if (Mathf.Floor(player.transform.position.x / mapSize.x) != playerTile.x || Mathf.Floor(player.transform.position.z / mapSize.y) != playerTile.y)
+        {
+            // Update playerTile
+            var oldPlayerTile = playerTile;
+            playerTile = new Vector2(Mathf.Floor(player.transform.position.x / mapSize.x), Mathf.Floor(player.transform.position.z / mapSize.y));
+            var dirTile = playerTile - oldPlayerTile;
+
+            // Destroy TerrainTiles die zich buiten een 3x3 grid om de speler bevinden.
+            var tiles = FindObjectsOfType<TerrainTile>();
+            foreach (TerrainTile terrainTile in tiles)
+            {
+                var localCoords = playerTile - terrainTile.tile;
+                if (Mathf.Abs(localCoords.x) > 1 || Mathf.Abs(localCoords.y) > 1)
+                {
+                    Destroy(terrainTile.gameObject);
+                }
+            }
+
+            // Maak nieuwe tiles aan in de richting die de speler heeft genomen.
+            if (dirTile.x != 0)
+            {
+                for (int y = -1; y <= 1; y++)
+                {
+                    GenerateMesh(new Vector2(playerTile.x + dirTile.x, playerTile.y+ y));
+                }
+            }
+
+            if (dirTile.y != 0)
+            {
+                for (int x = -1; x <= 1; x++)
+                {
+                    GenerateMesh(new Vector2(playerTile.x + x, playerTile.y + dirTile.y));
+                }
+            }
+        }
     }
 
-    float[,] GenerateHeights()
+    void GenerateMesh(Vector2 coords)
     {
-        float[,] heights = new float[width, height];
-        for (int x = 0; x < width; x++)
+        int xOrg = (int)coords.x;
+        int yOrg = (int)coords.y;
+
+        float[,] Heights = new float[(int)mapSize.x + 1, (int)mapSize.y + 1];
+        Heights = CalcNoise(xOrg, yOrg);
+        TerrainData NewTerrain = new TerrainData();
+
+        NewTerrain.heightmapResolution = (int)mapSize.x;
+        NewTerrain.size = new Vector3(mapSize.x, maxHeight, mapSize.y);
+        NewTerrain.baseMapResolution = (int)mapSize.x / 2;
+        NewTerrain.SetDetailResolution(1024, 16);
+        NewTerrain.SetHeights(0, 0, Heights);
+
+        // Maak nieuwe terrain tile
+        GameObject TerrainSquare = Terrain.CreateTerrainGameObject(NewTerrain);
+        TerrainSquare.transform.position = new Vector3((xOrg * mapSize.x), 0, (yOrg * mapSize.y));
+
+        // Voeg TerrainTile script toe en geef de tile de juiste coördinaten
+        TerrainSquare.AddComponent<TerrainTile>();
+        TerrainSquare.GetComponent<TerrainTile>().tile = new Vector2(xOrg, yOrg);
+    }
+    
+    // Genereer Height Data aan de hand van Perlin Noise
+    float[,] CalcNoise(int xOrg, int yOrg)
+    {
+        float[,] finalheight = new float[(int)mapSize.x + 1, (int)mapSize.y + 1];
+
+        float y = 0.0f;
+        while (y < (int)mapSize.y + 1)
         {
-            for (int y = 0; y < height; y++)
+            float x = 0.0f;
+            while (x < (int)mapSize.x + 1)
             {
-                heights[x, y] = CalculateHeight(x, y);
+                float Xcoord = xOrg * mapSize.x + (x - 1);
+                float Ycoord = yOrg * mapSize.y + (y - 1);
+                float ColorSample = PerlinNoise(Xcoord, Ycoord);
+
+                finalheight[(int)y, (int)x] = 0 + (ColorSample);
+
+                x++;
+            }
+
+            y++;
+        }
+
+        return finalheight;
+    }
+
+    // Zoek Perlin Noise aan de hand van coördinaten
+    public float PerlinNoise(float x, float y)
+    {
+        float xCoord = x / (mapSize.x) * scale + noiseOffset.x;
+        float yCoord = y / (mapSize.y) * scale + noiseOffset.y;
+        return Mathf.PerlinNoise(xCoord, yCoord);
+    }
+
+    // Destory de TerrainTile met de passende tile coördinaten
+    void DestroyTile(Vector2 coords)
+    {
+        var tiles = FindObjectsOfType<TerrainTile>();
+
+        foreach(TerrainTile terrainTile in tiles)
+        {
+            if (terrainTile.tile == coords)
+            {
+                Destroy(terrainTile.gameObject);
+            }
+        }
+    }
+
+    private TerrainTile FindTile(Vector2 coords)
+    {
+        var tiles = FindObjectsOfType<TerrainTile>();
+        var tile = new TerrainTile();
+
+        foreach (TerrainTile terrainTile in tiles)
+        {
+            if (terrainTile.tile == coords)
+            {
+                tile = terrainTile;
             }
         }
 
-        return heights;
-    }
-
-    // Stop Coördinaten in perlin noise map
-    float CalculateHeight(int x, int y)
-    {
-        float xCoord = (float)x / width * scale + offsetX;
-        float yCoord = (float)y / height * scale + offsetY;
-
-        return Mathf.PerlinNoise(xCoord, yCoord);
+        return tile;
     }
 }
